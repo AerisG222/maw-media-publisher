3790021a4ad2896956091a36abecd53feddc54ff
Mike Morano <mmorano@mikeandwan.us>
2025-10-25 10:44:51 -0400

    update command to run async and build out structure for processing files while showing a progress bar

--- src/MawMediaPublisher/Commands/FullProcessCommand.cs
+++ src/MawMediaPublisher/Commands/FullProcessCommand.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using MawMediaPublisher.Finder;
 using MawMediaPublisher.Models;
 using Spectre.Console;
 using Spectre.Console.Cli;
@@ -6,10 +7,11 @@ using Spectre.Console.Cli;
 namespace MawMediaPublisher.Commands;
 
 internal sealed class FullProcessCommand
-    : Command<FullProcessCommand.Settings>
+    : AsyncCommand<FullProcessCommand.Settings>
 {
     const int STATUS_SUCCESS = 0;
     const int STATUS_USER_CANCELLED = 1;
+    static readonly Lock _lock = new();
 
     public sealed class Settings
         : CommandSettings
@@ -32,7 +34,7 @@ internal sealed class FullProcessCommand
         public bool Interactive { get; init; }
     }
 
-    public override int Execute(
+    public async override Task<int> ExecuteAsync(
         CommandContext context,
         Settings settings
     )
@@ -43,25 +45,142 @@ internal sealed class FullProcessCommand
             settings.EffectiveDate
         );
 
-        if (settings.Interactive)
+        if (settings.Interactive && !PrintParametersAndContinue(category))
         {
-            OutputHeader("Parameters");
-            OutputVariable(" Category Name", category.Name);
-            OutputVariable("Effective Date", category.EffectiveDate.ToString("yyyy-mm-dd"));
-            OutputVariable("  Media Source", category.SourceDirectory);
-            OutputVariable(" Base Web Path", category.BaseWebPath);
-
-            AnsiConsole.WriteLine();
-
-            if (!AnsiConsole.Prompt(new ConfirmationPrompt("Continue?")))
-            {
-                return STATUS_USER_CANCELLED;
-            }
+            return STATUS_USER_CANCELLED;
         }
 
-        // do work here
+        var mediaFinder = new MediaFinder();
+        var foundFiles = mediaFinder.FindMedia(category.SourceDirectory);
+
+        if (settings.Interactive && !PrintMediaToProcessAndContinue(foundFiles))
+        {
+            return STATUS_USER_CANCELLED;
+        }
+
+        category.Media = foundFiles.Media;
+
+        await ProcessCategoryMedia(category);
 
         return STATUS_SUCCESS;
+    }
+
+    static async Task ProcessCategoryMedia(Category category)
+    {
+        await AnsiConsole.Progress()
+            .Columns([
+                new TaskDescriptionColumn(),
+                new ProgressBarColumn(),
+                new PercentageColumn(),
+                new SpinnerColumn(),
+            ])
+            .StartAsync(async ctx =>
+            {
+                var pctPerFile = (1.0 / category.Media.Count()) * 100;
+                var task = ctx.AddTask("[green]Processing Media[/]");
+
+                // from the migration app:
+                // when processing highest res image to full hi res destination, we kept crashing
+                // as we ran out of mem - for a single run, it looks like 1 image can take upwards of 5gb,
+                // so set max parallelism to 12 to try and stay below our sys mem (currently 64gb)
+                var opts = new ParallelOptions
+                {
+                    MaxDegreeOfParallelism = 12
+                };
+
+                await Parallel.ForEachAsync(category.Media, opts, async (media, token) =>
+                {
+                    await ProcessMedia(media);
+
+                    lock (_lock)
+                    {
+                        task.Increment(pctPerFile);
+                    }
+                });
+            });
+    }
+
+    static async Task ProcessMedia(MediaFile file)
+    {
+        await Task.Delay(1000);
+    }
+
+    static bool PrintParametersAndContinue(Category category)
+    {
+        OutputHeader("Parameters");
+        OutputVariable(" Category Name", category.Name);
+        OutputVariable("Effective Date", category.EffectiveDate.ToString("yyyy-MM-dd"));
+        OutputVariable("  Media Source", category.SourceDirectory);
+        OutputVariable(" Base Web Path", category.BaseWebPath);
+
+        AnsiConsole.WriteLine();
+
+        return ContinuePrompt();
+    }
+
+    static bool PrintMediaToProcessAndContinue(FindResults files)
+    {
+        var grid = new Grid();
+
+        grid.AddColumn();
+        grid.AddColumn();
+        grid.AddColumn();
+        grid.AddColumn();
+        grid.AddColumn();
+
+        grid.AddRow([
+            new Text("UNK", new Style(Color.Yellow, decoration: Decoration.Bold)),
+            new Text("Type", new Style(Color.Yellow, decoration: Decoration.Bold)),
+            new Text("Original", new Style(Color.Yellow, decoration: Decoration.Bold)),
+            new Text("Processing", new Style(Color.Yellow, decoration: Decoration.Bold)),
+            new Text("Support", new Style(Color.Yellow, decoration: Decoration.Bold)),
+        ]);
+
+        foreach (var file in files.Media)
+        {
+            grid.AddRow([
+                new Text("", new Style(Color.Green)),
+                new Text(MediaTypeDisplayForGrid(file.MediaType), new Style(Color.Green)),
+                new Text(Path.GetFileName(file.OriginalFilepath), new Style(Color.Green)),
+                new Text(Path.GetFileName(file.ProcessingFilepath) ?? "", new Style(Color.Green)),
+                new Text(Path.GetFileName(file.SupportFilepath) ?? "", new Style(Color.Green)),
+            ]);
+        }
+
+        foreach (var file in files.Unknown)
+        {
+            grid.AddRow([
+                new Text("!!", new Style(Color.Red)),
+                new Text(""),
+                new Text(Path.GetFileName(file), new Style(Color.Red)),
+                new Text(""),
+                new Text(""),
+            ]);
+        }
+
+        OutputHeader("Files to Process");
+        AnsiConsole.Write(grid);
+        AnsiConsole.WriteLine();
+
+        if (files.Unknown.Any())
+        {
+            AnsiConsole.MarkupLine("[red]!! Unknown files will not be processed !![/]");
+            AnsiConsole.WriteLine();
+        }
+
+        return ContinuePrompt();
+    }
+
+    static string MediaTypeDisplayForGrid(MediaType mt) => mt switch
+    {
+        MediaType.Image => "p",
+        MediaType.Video => "v",
+        _ => "?"
+    };
+
+    static bool ContinuePrompt()
+    {
+        return AnsiConsole.Prompt(new ConfirmationPrompt("Continue?"));
     }
 
     static void OutputVariable(string name, string value)
